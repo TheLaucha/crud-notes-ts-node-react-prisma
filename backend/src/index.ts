27641351e539aa221138899e3645acb141ebfbac
Lautaro <espinillo.lautaro@gmail.com>
2024-03-01 22:07:28 -0300

    Server configured and running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hola mundo")
+require("dotenv").config()
+import express, { Request, Response } from "express"
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hola mundos")
+})
+
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`)
+})
